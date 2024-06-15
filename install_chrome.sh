d729af471a8c8d3dac714c777b58f628e8bea034
Heerajeth <154071435+Candyshopowner@users.noreply.github.com>
2024-06-15 23:51:31 +0530

    Add files via upload

---
+++ install_chrome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+apt-get update
+apt-get install -y google-chrome-stable
+apt-get install -y chromium-chromedriver
